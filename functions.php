7bdb02cc704283ec83be27498c3f6492056978d0
mbrothen <mbrothen@gmail.com>
2022-01-15 17:36:41 -0600

    initial commit

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+    function theme_files(){
+        wp_enqueue_script('theme_javascript', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+        wp_enqueue_style('theme_style', get_theme_file_uri('/build/style-index.css'));
+        wp_enqueue_style('theme_extra_style', get_theme_file_uri('/build/index.css'));
+        wp_enqueue_style('font_awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+        wp_enqueue_style('external_fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i
+        ');
+    }
+    add_action('wp_enqueue_scripts', 'theme_files');
+    function theme_features(){        
+        register_nav_menu('headerMenuLocation', 'Header Menu Location');
+        register_nav_menu('footerLocationOne', 'Footer Location One');
+        register_nav_menu('footerLocationTwo', 'Footer Location Two');
+        add_theme_support('title-tag');
+    }
+    add_action('after_setup_theme', 'theme_features');
+?>
